9b9ad6bae9f1942b557f3e39c9fb9e788828f311
PingOfDeathSA <ronaldnt8@gmail.com>
2023-03-31 19:01:47 +0200

    login updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(session({
   secret: 'ThETerminatorIsHere',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false }
 }));
 
 app.use(passport.initialize());
